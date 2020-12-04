f237a622b1f37743a3818462977e38ad2858db4e
Maria Vitoria Durazzo <marividurazzo007@gmail.com>
2020-12-04 17:06:29 -0200

    api e view inicial

---
+++ ProAgil.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
